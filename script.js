095af90093a5bd7619a733d3cc5f5e6152ba6ae0
Itz Primzy <prospermaduabuchi88@gmail.com>
2025-02-02 04:51:44 -0800

    Add files via upload
    I added this file

---
+++ script.js
@@ -0,0 +1,5 @@
+        
+        alert("Hello Everyone and welcome tO SUCCESSLAND XTAR SCHOOL")
+        function myFunc(){
+        alert("You can't view our activities if you are not a member of SXS please fill in the form in the register button if you are a member and also fill in the form for registration. Thank YOU")
+        }
